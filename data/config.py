b9bae0a31a9c6492dd61b1c86ee3c3fdf5c25754
AnanayVikramGupta <134192937+AnanayVikramGupta@users.noreply.github.com>
2023-09-11 10:38:01 -0400

    Update config.py
    fix config

--- data/config.py
+++ data/config.py
@@ -762,7 +762,7 @@ yolact_darknet53_config = yolact_base_config.copy({
 yolact_resnet50_config = yolact_base_config.copy({
     'name': 'yolact_resnet50',
 
-    'backbone': resnet50_backbone_2.copy({
+    'backbone': resnet50_backbone.copy({
         'selected_layers': list(range(1, 4)),
         
         'pred_scales': yolact_base_config.backbone.pred_scales,
@@ -780,7 +780,7 @@ kaggle_resnet50_2 = yolact_resnet50_config.copy({
     'dataset': kaggle_dataset_coco,
     'num_classes': len(kaggle_dataset_coco.class_names) + 1,
 
-    'backbone': resnet50_backbone.copy({
+    'backbone': resnet50_backbone_2.copy({
         'selected_layers': list(range(1, 4)),
         
         'pred_scales': yolact_base_config.backbone.pred_scales,
